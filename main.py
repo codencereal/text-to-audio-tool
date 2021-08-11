1ab8ae9c513f8c91a94b6d2fbb5cedcb5d56decf
Jaidon Nelson <jaidonkdnelson@gmail.com>
2021-08-10 21:16:12 -0400

    Add text cli argument

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import pdfplumber
 from gtts import gTTS
 import argparse
 
-text = "This is just some test text to ensure that the gTTS package is working."
+parser = argparse.ArgumentParser(description='Convert a book or text file into a text to speech audiobook')
+parser.add_argument('-t', '--text', metavar='', help='text to be converted to audio')
+args = parser.parse_args()
+
+text = args.text
+print("Text extracted!")
 
 language = 'en'
 gtts_transformer = gTTS(text=text, lang=language)
 gtts_transformer.save("test.mp3")
-print("File converted")
+print("File converted!")
